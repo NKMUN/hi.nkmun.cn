bcf17ad8dee6339107552c54a68749fb346a31f1
wacky6.AriesMBP <416707889@qq.com>
2017-02-10 20:58:17 +0800

    isolate root admin -> /root/

--- src/router/role-dispatch.js
+++ src/router/role-dispatch.js
@@ -2,7 +2,7 @@ export default function getRoleRoute(role) {
   switch (role) {
     case 'school': return '/school/'
     case 'admin':  return '/admin/'
-    case 'root':   return '/admin/'
+    case 'root':   return '/root/'
     default:       return '/not-found'
   }
 }
