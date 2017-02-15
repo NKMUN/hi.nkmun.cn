3db2b40912e20035e2a58750c214085938eb2e80
wacky6.AriesMBP <416707889@qq.com>
2017-02-16 03:14:30 +0800

    fix Logout: clear stored token

--- src/components/Logout.vue
+++ src/components/Logout.vue
@@ -3,10 +3,12 @@
 </template>
 
 <script>
+import { clearToken } from 'persistence/token'
 export default {
   name: 'logout',
   mounted() {
     this.$store.commit('user/token', null)
+    clearToken()
     this.$router.replace('/')
   }
 }
