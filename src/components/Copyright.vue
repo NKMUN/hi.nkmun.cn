dece4d438c13e5a43e7e2ff8e14350e9b8918a08
darkphooo <dark.xiaoyao@qq.com>
2017-02-26 22:25:17 +0800

    change copyright

--- src/components/Copyright.vue
+++ src/components/Copyright.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="copyright">
-    <pre>2016-2017 (C) NKMUN</pre>
+    <pre>Copyright ©2011-2017 Nanking international model united nations conference for high school students, All Rights Reserved
+    汇文国际中学生模拟联合国大会组委会 版权所有 苏ICP备15062651号</pre>
   </div>
 </template>
 
