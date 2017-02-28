5d242c5f54ad3e54b7fe435a4bdf058efa020700
wacky6.AriesMBP <416707889@qq.com>
2017-02-28 21:59:14 +0800

    guard invalid token

--- src/store/user.js
+++ src/store/user.js
@@ -1,5 +1,9 @@
 function jwtBody(token) {
-  return JSON.parse( atob( token.split('.')[1] ) )
+  try {
+    return JSON.parse( atob( token.split('.')[1] ) )
+  }catch(e){
+    return null
+  }
 }
 
 export default {
