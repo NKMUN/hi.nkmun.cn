cfbb0e7c9d25e164e9a36a249cf1df7bb956c3ee
wacky6.AriesMBP <416707889@qq.com>
2017-03-01 13:38:48 +0800

    fix jwt decoding

--- src/store/user.js
+++ src/store/user.js
@@ -1,46 +1,4 @@
-import Base64 from 'base64-js'
-
-function Utf8ArrayToStr(array) {
-  var out, i, len, c;
-  var char2, char3;
-
-  out = "";
-  len = array.length;
-  i = 0;
-  while(i < len) {
-  c = array[i++];
-  switch(c >> 4)
-  {
-    case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
-      // 0xxxxxxx
-      out += String.fromCharCode(c);
-      break;
-    case 12: case 13:
-      // 110x xxxx   10xx xxxx
-      char2 = array[i++];
-      out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
-      break;
-    case 14:
-      // 1110 xxxx  10xx xxxx  10xx xxxx
-      char2 = array[i++];
-      char3 = array[i++];
-      out += String.fromCharCode(((c & 0x0F) << 12) |
-                     ((char2 & 0x3F) << 6) |
-                     ((char3 & 0x3F) << 0));
-      break;
-  }
-  }
-
-  return out;
-}
-
-function jwtBody(token) {
-  try {
-    return JSON.parse( Utf8ArrayToStr( Base64.toByteArray( token.split('.')[1] ) ) )
-  }catch(e){
-    return null
-  }
-}
+const decode = require('jwt-decode')
 
 export default {
   namespaced: true,
@@ -61,7 +19,7 @@ export default {
     token( state, token ) {
       if (token) {
         state.token = token
-        state.payload = jwtBody(token)
+        state.payload = decode(token)
       }else {
         state.token = null
         state.payload = {}
