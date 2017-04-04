ee7fdcc8a7f40d463ebe64e822762eb56eda265f
wacky6.AriesMBP <416707889@qq.com>
2017-04-04 13:32:53 +0800

    School/Payment-1: fix text

--- src/components/School/Payment-1.vue
+++ src/components/School/Payment-1.vue
@@ -30,7 +30,7 @@
       >
         <i class="el-icon-upload"></i>
         <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过1MB</div>
+        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过2MB</div>
       </el-upload>
 
       <el-alert v-if="beforeUploadError" type="error" :title="beforeUploadError" :closable="false" />
