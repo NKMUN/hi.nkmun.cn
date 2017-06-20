44c1ffbb654aee5c11349f6b285982fe50beecf7
wacky6.AriesMBP <416707889@qq.com>
2017-06-20 19:03:06 +0800

    impl committee export

--- src/components/Admin/Export.vue
+++ src/components/Admin/Export.vue
@@ -27,13 +27,25 @@
       :disabled="busy"
     > 付款信息 </el-button>
 
+    <el-button
+      @click="exportCsv('committees')"
+      type="primary"
+      :disabled="busy"
+    > 组委信息 </el-button>
+
+    <el-button
+      @click="exportZip('committees/photos')"
+      type="primary"
+      :disabled="busy"
+    > 组委照片 </el-button>
+
   </div>
 
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import saveAsFile from 'lib/save-as-file'
+import {saveAsFile, downloadFile} from 'lib/save-as-file'
 
 export default {
   name: 'export',
@@ -55,6 +67,25 @@ export default {
                   .responseType('blob')
                   .set( ... this.authorization )
         saveAsFile(blob, 'NKMUN-'+type+'.csv')
+      } catch(e) {
+        this.$notify({
+          type: 'warning',
+          title: '未能导出',
+          message: e.message,
+          duration: 0
+        })
+      } finally {
+        this.busy = false
+      }
+    },
+    async exportZip(type) {
+      this.busy = true
+      try {
+        let {
+          header
+        } = await this.$agent.get('/api/export/'+type)
+                  .set( ... this.authorization )
+        downloadFile('/api/export/'+type+header.location, 'NKMUN-'+type.replace(/-/g, '-')+'.zip')
       } catch(e) {
         this.$notify({
           type: 'warning',
--- src/lib/save-as-file.js
+++ src/lib/save-as-file.js
@@ -1,18 +1,22 @@
 // ref: http://stackoverflow.com/questions/14964035/how-to-export-javascript-array-info-to-csv-on-client-side
 
-function saveAsFile(blob, filename) {
+const downloadFile = (url, filename) => {
+  let link = document.createElement('a')
+  link.setAttribute('href', url)
+  link.setAttribute('download', filename)
+  link.style.display = 'none'
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+}
+
+const saveAsFile = (blob, filename) => {
   if (navigator.msSaveBlob) {
     navigator.msSaveBlob(blob, filename)
   } else {
-    let link = document.createElement('a')
-    let url = URL.createObjectURL(blob)
-    link.setAttribute('href', url)
-    link.setAttribute('download', filename)
-    link.style.display = 'none'
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
+    downloadFile(URL.createObjectURL(blob), filename)
   }
 }
 
+export { downloadFile, saveAsFile }
 export default saveAsFile
