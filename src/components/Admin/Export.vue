f29577f85b9d5662bf6dc9846142343fdde4efaa
wacky6.AriesMBP <416707889@qq.com>
2017-09-16 18:34:50 +1000

    Admin/Export: add icons

--- src/components/Admin/Export.vue
+++ src/components/Admin/Export.vue
@@ -7,43 +7,43 @@
       @click="exportCsv('seats')"
       type="primary"
       :disabled="busy"
-    > 名额分配 </el-button>
+    > <icon name="table"/> 名额分配 </el-button>
 
     <el-button
       @click="exportCsv('leaders')"
       type="primary"
       :disabled="busy"
-    > 领队信息 </el-button>
+    > <icon name="flag"/> 领队信息 </el-button>
 
     <el-button
       @click="exportCsv('representatives')"
       type="primary"
       :disabled="busy"
-    > 代表信息 </el-button>
+    > <icon name="id-card"/> 代表信息 </el-button>
 
     <el-button
       @click="exportCsv('reservations')"
       type="primary"
       :disabled="busy"
-    > 住宿信息 </el-button>
+    > <icon name="bed"/> 住宿信息 </el-button>
 
     <el-button
       @click="exportCsv('billings')"
       type="primary"
       :disabled="busy"
-    > 付款信息 </el-button>
+    > <icon name="jpy"/> 付款信息 </el-button>
 
     <el-button
       @click="exportCsv('committees')"
       type="primary"
       :disabled="busy"
-    > 组委信息 </el-button>
+    > <icon name="users"/> 组委信息 </el-button>
 
     <el-button
       @click="exportZip('committees/photos')"
       type="primary"
       :disabled="busy"
-    > 组委照片 </el-button>
+    > <icon name="photo"/> 组委照片 </el-button>
 
   </div>
 
@@ -52,9 +52,20 @@
 <script>
 import { mapGetters } from 'vuex'
 import {saveAsFile, downloadFile} from '@/lib/save-as-file'
+import Icon from 'vue-awesome'
+import 'vue-awesome/icons/table'
+import 'vue-awesome/icons/users'
+import 'vue-awesome/icons/photo'
+import 'vue-awesome/icons/flag'
+import 'vue-awesome/icons/id-card'
+import 'vue-awesome/icons/jpy'
+import 'vue-awesome/icons/bed'
 
 export default {
   name: 'export',
+  components: {
+    Icon
+  },
   computed: {
     ... mapGetters({
       authorization: 'user/authorization'
@@ -115,4 +126,9 @@ export default {
     flex-shrink: 0
 .el-button
   margin: .5em 0
+.fa-icon
+  vertical-align: bottom
+  opacity: 0.8
+  width: 2ch
+  margin-right: .5ch
 </style>
