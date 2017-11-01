253d74d0a72f4d08f0896b233082a20d422a7563
wacky6.AriesMBP <416707889@qq.com>
2017-11-01 19:59:35 +1100

    new payment credentials

--- src/components/School/components/PaymentMethods.vue
+++ src/components/School/components/PaymentMethods.vue
@@ -1,25 +1,34 @@
 <template>
   <el-tabs v-model="pane" class="tabs">
 
-    <el-tab-pane label="银行转账" name="bank">
-
-      <p>请选择一个银行账号进行转账：</p>
-
+    <el-tab-pane label="工商银行" name="icbc">
+      <p>请使用以下账号转账，或使用工商银行客户端扫描二维码：</p>
       <BankTransferCred
         card="6212264301018811073"
         name="王亚炜"
         branch="工商银行南京月牙湖支行"
       />
+      <img alt="工商银行二维码" width="300" src="../../../assets/icbc-qr.jpg"><img>
+    </el-tab-pane>
+
+    <el-tab-pane label="建设银行" name="bankcomm">
+      <p>请使用以下账号转账，或使用建设银行客户端扫描二维码：</p>
       <BankTransferCred
         card="6236691370000065362"
         name="王亚炜"
         branch="建设银行南京中山门支行"
       />
+      <img alt="建设银行二维码" width="300" src="../../../assets/bankcomm-qr.jpg"><img>
+    </el-tab-pane>
+
+    <el-tab-pane label="中国银行" name="boc">
+      <p>请使用以下账号转账，或使用中国银行客户端扫描二维码：</p>
       <BankTransferCred
         card="6217856100007698647"
         name="王亚炜"
         branch="中国银行南京梅花山庄支行"
       />
+      <img alt="中国银行二维码" width="300" src="../../../assets/boc-qr.jpg"><img>
     </el-tab-pane>
 
     <el-tab-pane label="支付宝" name="alipay">
@@ -42,7 +51,7 @@ export default {
     BankTransferCred
   },
   data: () => ({
-    pane: 'bank'
+    pane: 'alipay'
   })
 }
 </script>
