8cef9252b42fddd6fe1edb863e85211fe68b7cef
wacky6.AriesMBP <416707889@qq.com>
2017-12-04 18:58:32 +1100

    [Breaking Change]: migrate to token based export

--- src/components/Admin/Export.vue
+++ src/components/Admin/Export.vue
@@ -4,67 +4,67 @@
     <h3>导出</h3>
 
     <el-button
-      @click="exportCsv('applications/seats')"
+      @click="exportAs('applications/seats', '.csv')"
       type="primary"
       :disabled="busy"
     > <icon name="wpforms"/> 报名 - 名额分配 </el-button>
 
     <el-button
-      @click="exportCsv('applications/contacts')"
+      @click="exportAs('applications/contacts', '.csv')"
       type="primary"
       :disabled="busy"
     > <icon name="phone"/> 报名 - 联系人 </el-button>
 
     <el-button
-      @click="exportCsv('seats')"
+      @click="exportAs('seats', '.csv')"
       type="primary"
       :disabled="busy"
     > <icon name="table"/> 当前名额 </el-button>
 
     <el-button
-      @click="exportCsv('leaders')"
+      @click="exportAs('leaders', '.csv')"
       type="primary"
       :disabled="busy"
     > <icon name="flag"/> 领队信息 </el-button>
 
     <el-button
-      @click="exportCsv('representatives')"
+      @click="exportAs('representatives', '.csv')"
       type="primary"
       :disabled="busy"
     > <icon name="id-card"/> 代表信息 </el-button>
 
     <el-button
-      @click="exportCsv('reservations')"
+      @click="exportAs('reservations', '.csv')"
       type="primary"
       :disabled="busy"
     > <icon name="bed"/> 住宿信息 </el-button>
 
     <el-button
-      @click="exportCsv('billings')"
+      @click="exportAs('billings', '.csv')"
       type="primary"
       :disabled="busy"
     > <icon name="jpy"/> 付款信息 </el-button>
 
     <el-button
-      @click="exportCsv('committees')"
+      @click="exportAs('committees', '.csv')"
       type="primary"
       :disabled="busy"
     > <icon name="users"/> 组委信息 </el-button>
 
     <el-button
-      @click="exportZip('committees/photos')"
+      @click="exportAs('committees/photos', '.zip')"
       type="primary"
       :disabled="busy"
     > <icon name="photo"/> 组委照片 </el-button>
 
     <el-button
-      @click="exportCsv('volunteers')"
+      @click="exportAs('volunteers', 'csv')"
       type="primary"
       :disabled="busy"
     > <icon name="handshake-o"/> 志愿者信息 </el-button>
 
     <el-button
-      @click="exportCsv('daises')"
+      @click="exportAs('daises', '.csv')"
       type="primary"
       :disabled="busy"
     > <icon name="user-secret"/> 会场主席信息 </el-button>
@@ -74,7 +74,7 @@
 </template>
 
 <script>
-import {saveAsFile, downloadFile} from '@/lib/save-as-file'
+import { downloadFile } from '@/lib/save-as-file'
 import 'vue-awesome/icons/wpforms'
 import 'vue-awesome/icons/phone'
 import 'vue-awesome/icons/table'
@@ -93,29 +93,13 @@ export default {
     busy: false,
   }),
   methods: {
-    async exportCsv(type) {
-      this.busy = true
-      try {
-        const blob = await this.$agent.get('/api/export/'+type).blob()
-        saveAsFile(blob, 'NKMUN-'+type+'.csv')
-      } catch(e) {
-        this.$notify({
-          type: 'warning',
-          title: '未能导出',
-          message: e.message,
-          duration: 0
-        })
-      } finally {
-        this.busy = false
-      }
-    },
-    async exportZip(type) {
+    async exportAs(type, extension) {
       this.busy = true
       try {
         let {
           header
         } = await this.$agent.get('/api/export/'+type)
-        downloadFile('/api/export/'+type+header.location, 'NKMUN-'+type.replace(/-/g, '-')+'.zip')
+        downloadFile('/api/export/'+type+header.location, 'NKMUN-'+type.replace(/\//g, '-')+extension)
       } catch(e) {
         this.$notify({
           type: 'warning',
