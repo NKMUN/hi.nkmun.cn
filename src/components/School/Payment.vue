20beed45ccb00c46923df60b9a1a547c0ebac4cb
wacky6.AriesMBP <416707889@qq.com>
2018-02-04 00:51:22 +0800

    <Breaking> School/Payment: redesign interaction
    breaks API:
      POST schools/~/payments
      POST schools/~/progress
    
    requires backend commit > 79c724ac47ee186a17eba2f364f3baf2d725005b

--- src/components/School/Payment.vue
+++ src/components/School/Payment.vue
@@ -6,42 +6,40 @@
       class="payment-detail"
       :school="school"
       :round="round"
-      @loaded="billingLoaded=true"
+      @loaded="billsLoaded=true"
     />
 
-    <PaymentMethods class="payment-methods" v-if="billingLoaded" />
+    <PaymentMethods class="payment-methods" v-if="billsLoaded" />
 
-    <div class="upload" v-if="billingLoaded" >
-
+    <div class="upload" v-if="billsLoaded">
       <h4>凭证上传</h4>
 
-      <div class="note">
-        <ul>
-          <li>银行转帐回执（照片） 或 支付转账截图</li>
-          <li>请在这张图片中包含：<b>转账人、金额、时间</b></li>
-        </ul>
-      </div>
+      <ul class="note">
+        <li>可上传：银行回执（照片） 或 App内转账截图</li>
+        <li>请包含：<b>转账人、金额、时间</b></li>
+        <li class="constraint-text">只能上传 <b>jpg / png</b> 文件，每个文件不超过 <b>2MB</b></li>
+      </ul>
 
       <el-upload
-        drag
-        name="file"
-        :action="uploadUrl"
-        :on-success="onSuccess"
-        :on-error="onError"
-        :before-upload="beforeUpload"
-        :headers="{ Authorization: 'Bearer ' + token }"
-        :show-file-list="false"
-        :multiple="false"
+        class="el-upload-wrapper"
+        action="/api/images"
         accept="image/jpeg, image/png, .jpg, .jpeg, .png"
+        list-type="picture-card"
+        :file-list="uploadedImages"
+        :on-preview="handlePictureCardPreview"
+        :on-success="handleSuccess"
+        :on-remove="handleRemove"
+        :before-upload="beforeUpload"
       >
-        <i class="el-icon-upload"/>
-        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过2MB</div>
+        <i class="el-icon-plus" />
       </el-upload>
 
-      <el-alert v-if="beforeUploadError" type="error" :title="beforeUploadError" :closable="false" />
-    </div>
+      <el-dialog v-model="previewVisible" title="预览">
+        <img class="preview-image" :src="previewImageUrl" />
+      </el-dialog>
 
+      <el-button size="large" type="primary" @click="handleSubmit" :busy="busy"> 提交 </el-button>
+    </div>
   </div>
 </template>
 
@@ -52,6 +50,9 @@ import PaymentMethods from './components/PaymentMethods'
 import BillingDetail from './components/BillingDetail'
 import store from '@/store/index'
 import roundText from '@/lib/round-text'
+
+const PREVIEW_IMAGE_SIZE_QUERY = "size=tiny"
+const PREVIEW_DIALOG_IMAGE_SIZE_QUERY = "size=medium"
 
 export default {
   name: 'payment',
@@ -69,45 +70,110 @@ export default {
     authorization() {
       return `Bearer ${this.$store.getters['user/token']}`
     },
-    uploadUrl() {
-      return '/api/schools/'+this.school+'/payments/'
-    },
+    uploadedImages() {
+      return this.receipts.map((id, idx) => ({
+        name: String(idx),
+        url: `/api/images/${id}?size=tiny&format=jpg`
+      }))
+    }
   },
   data: () => ({
-    beforeUploadError: null,
-    billingLoaded: false
+    billsLoaded: false,
+    receipts: [],
+    busy: false,
+    previewImageUrl: '',
+    previewVisible: false
   }),
   methods: {
-    async onSuccess(res, file, list) {
-      this.$notify({
-        type: 'success',
-        title: '上传成功',
-        message: '请等待组委审核',
-        duration: 5000
-      })
-
-      this.$router.replace('/school/')
-      this.$store.commit('school/stage', `${this.round}.paid`)
+    async fetchUploadedReceipts() {
+      const payments = await this.$agent
+        .get(`/api/schools/${this.school}/payments/`)
+        .query({ round: this.round })
+        .body()
+      this.receipts = payments.reduce(
+        (ret, cur) => [
+          ...ret,
+          ...(cur.images || [])
+        ],
+        []
+      )
     },
-    onError(err, file, list) {
-      this.$notify({
-        type: 'error',
-        title: '上传失败，请稍后再试。',
-        message: err.message,
-        duration: 0
-      })
+    handleSuccess(resp, file, fileList) {
+      return this.handleFileList(fileList)
+    },
+    handleRemove(file, fileList) {
+      return this.handleFileList(fileList)
+    },
+    handleFileList(fileList) {
+      const recoverIdFromUrl = url => {
+        const API_IMAGE_PREFIX = '/api/images/'
+        if (url.startsWith(API_IMAGE_PREFIX)) {
+          const startPos = API_IMAGE_PREFIX.length
+          const queryPos = url.indexOf('?')
+          const endPos = queryPos === -1 ? undefined : queryPos    // js does not offer C style substr
+          return url.slice(startPos, endPos)
+        }
+      }
+      const files = fileList.filter(file => file.status === 'success')
+      this.receipts = files.map(file => file.response ? file.response.id : recoverIdFromUrl(file.url))
+    },
+    handlePictureCardPreview(file) {
+      this.previewImageUrl = file.url.replace(PREVIEW_IMAGE_SIZE_QUERY, PREVIEW_DIALOG_IMAGE_SIZE_QUERY)
+      this.previewVisible = true
     },
     beforeUpload(file) {
-      this.beforeUploadError = false
       if ( ! (file.type === 'image/jpeg' || file.type === 'image/png') ) {
-        this.beforeUploadError = '只能上传jpg或png格式'
+        this.$message({
+          message: '图片必须是 png 或 jpg 格式',
+          type: 'error'
+        })
         return false
       }
       if ( file.size / 1024 / 1024 > 2 ) {
-        this.beforeUploadError = '图片大小不能大于2MB'
+        this.$message({
+          message: '图片必须小于 2MB',
+          type: 'error'
+        })
         return false
       }
       return true
+    },
+    handleSubmit() {
+      if (!this.receipts || this.receipts.length === 0) {
+        this.$confirm('尚未上传转账凭证，确认提交？', '确认？', { type: 'warning' })
+            .then(
+              () => this.confirmReceipts(),
+              () => null // nop
+            )
+      } else {
+        this.confirmReceipts()
+      }
+    },
+    async confirmReceipts() {
+      this.busy = true
+      try {
+        await this.$agent.post(`/api/schools/${this.school}/payments/`).send({ images: this.receipts })
+        const {
+          body
+        } = await await this.$agent.post(`/api/schools/${this.school}/progress`).send({ confirmPayment: true })
+        this.$notify({
+          type: 'success',
+          title: '缴费凭证已提交',
+          message: '请等待审核',
+          duration: 5000
+        })
+        this.$router.replace('/school/')
+        this.$store.commit('school/stage', body.nextStage)
+      } catch(err) {
+        this.$notify({
+          type: 'error',
+          title: '提交失败，请稍后再试。',
+          message: err.message,
+          duration: 0
+        })
+      } finally {
+        this.busy = false
+      }
     }
   },
   filters: { roundText },
@@ -117,6 +183,9 @@ export default {
       next()
     else
       next('/school/')
+  },
+  mounted() {
+    this.fetchUploadedReceipts()
   }
 }
 </script>
@@ -127,10 +196,6 @@ export default {
   flex-vert: flex-start center
   *
     flex-shrink: 0
-  .note
-    white-space: nowrap
-    color: #475669
-    font-size: 14px
   .payment-methods
     width: 360px
   .payment-detail
@@ -138,7 +203,16 @@ export default {
     width: 80%
   h4
     text-align: center
-  .el-alert
-    margin-top: 1em
-    width: auto
+  .upload
+    flex-vert: flex-start center
+    .note
+      white-space: nowrap
+      color: #475669
+      font-size: 14px
+    .el-button
+      margin: 2em auto
+    .el-upload-wrapper
+      max-width: 624px    // 4 pics per row
+  .preview-image
+    width: 100%
 </style>
