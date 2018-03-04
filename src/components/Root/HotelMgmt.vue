4056f5b2a1ac09f7d1cb8798da1fc017d8bde76e
Jiewei Qian <qjw@wacky.one>
2018-03-04 20:19:53 +1100

    HotelMgmt: fix layout width

--- src/components/Root/HotelMgmt.vue
+++ src/components/Root/HotelMgmt.vue
@@ -18,10 +18,10 @@
     <el-table v-if="hotels" :data="hotels" class="hotel-table">
       <el-table-column prop="name" label="名称" min-width="160" sortable />
       <el-table-column prop="type" label="房型" width="96" sortable />
-      <el-table-column prop="notBefore" label="入住时间" width="96" sortable>
+      <el-table-column prop="notBefore" label="入住时间" width="108" sortable>
         <span slot-scope="{row}">{{ row.notBefore }}</span>
       </el-table-column>
-      <el-table-column prop="notAfter" label="退房时间" width="96" sortable>
+      <el-table-column prop="notAfter" label="退房时间" width="108" sortable>
         <span slot-scope="{row}">{{ row.notAfter }}</span>
       </el-table-column>
       <el-table-column prop="price" label="单价" width="64" />
