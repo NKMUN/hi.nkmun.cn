daeefd633122e39145a1d6f64525d196d09122ad
Jiewei Qian <qjw@wacky.one>
2018-03-04 20:30:59 +1100

    Academic-Director: add category name transform

--- src/components/AcademicDirector/lib/compute-categories.js
+++ src/components/AcademicDirector/lib/compute-categories.js
@@ -1,21 +1,34 @@
 import { generatePalettes } from '@/lib/color-palette'
 
+const getCategoryOrder = (cat) => {
+  if (cat.includes('通用')) return `0_${cat}`
+  if (cat.includes('中文')) return `1_${cat}`
+  if (cat.includes('英文')) return `2_${cat}`
+  return `3_${cat}`
+}
+
+const getCategoryName = (cat) => {
+  if (cat.includes('通用')) '综合'
+  return cat
+}
+
 function computeCategoriesFromTests(tests) {
   if (!tests) {
     return []
   } else {
     const palettes = generatePalettes(tests.length)
-    const categories = [...new Set(tests.map(test => test.tag))].sort()
-    return categories.map((cat, idx) => ({
-      id: cat,
-      name: cat,
-      palette: palettes[idx],
-      departments: [...new Set(tests
-        .filter(test => test.tag === cat)
-        .map(test => test.departments)
-        .reduce((s, v) => [...s, ...v], [])
-      )]
-    }))
+    return [...new Set(tests.map(test => test.tag))]
+      .sort((a, b) => getCategoryOrder(a).localeCompare(getCategoryOrder(b)))
+      .map((cat, idx) => ({
+        id: getCategoryName(cat),
+        name: getCategoryName(cat),
+        palette: palettes[idx],
+        departments: [...new Set(tests
+          .filter(test => test.tag === cat)
+          .map(test => test.departments)
+          .reduce((s, v) => [...s, ...v], [])
+        )]
+      }))
   }
 }
 
