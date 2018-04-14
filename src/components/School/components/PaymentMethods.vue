13d973bbc7eb47aa92bc08980614002fee10f494
Yifei Chu <dark.xiaoyao@live.com>
2018-04-14 14:23:35 +0800

    Change payment methods

--- src/components/School/components/PaymentMethods.vue
+++ src/components/School/components/PaymentMethods.vue
@@ -1,24 +1,13 @@
 <template>
-  <el-tabs v-model="pane" class="tabs">
-
-    <el-tab-pane label="工商银行" name="icbc">
-      <p>请使用以下账号转账，或使用工商银行客户端扫描二维码：</p>
-      <BankTransferCred
-        card="6212264301018811073"
-        name="王亚炜"
-        branch="工商银行南京月牙湖支行"
-      />
-      <img alt="工商银行二维码" width="300" src="../../../assets/icbc-qr.jpg"><img>
-    </el-tab-pane>
+  <el-tabs v-model="pane" class="tabs"> 
 
     <el-tab-pane label="建设银行" name="bankcomm">
-      <p>请使用以下账号转账，或使用建设银行客户端扫描二维码：</p>
+      <p>请使用以下账号转账：</p>
       <BankTransferCred
-        card="6236691370000065362"
+        card="6217001370034674176"
         name="王亚炜"
-        branch="建设银行南京中山门支行"
+        branch="建设银行城东支行"
       />
-      <img alt="建设银行二维码" width="300" src="../../../assets/bankcomm-qr.jpg"><img>
     </el-tab-pane>
 
     <el-tab-pane label="中国银行" name="boc">
@@ -32,7 +21,7 @@
     </el-tab-pane>
 
     <el-tab-pane label="支付宝" name="alipay">
-      <p>请用支付宝扫描以下二维码转账：</p>
+      <p>请用支付宝扫描以下二维码转账（由于支付宝提现额度限制，转账时请额外支付0.1%手续费）：</p>
       <img id="alipay" alt="支付宝二维码" width="300" src="../../../assets/nkmun-alipay-qr.jpg"><img>
     </el-tab-pane>
 
