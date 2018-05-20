1f2b728b8e4b04f3b66f348e3df60bd43b5ab33e
Yifei Chu <dark.xiaoyao@live.com>
2018-05-20 14:10:38 +0800

    update validate rules for mobile

--- src/components/form/Contact.vue
+++ src/components/form/Contact.vue
@@ -39,7 +39,7 @@
       prop="phone"
       :rules="[
         { required: true, message: '请输入手机号', trigger: 'blur'},
-        { type: 'string', pattern: /^(1[34578]\d{9}|\+\d{11,})$/, message: '手机号格式不正确', trigger: 'blur' }
+        { type: 'string', pattern: /^(1[3456789]\d{9}|\+\d{11,})$/, message: '手机号格式不正确', trigger: 'blur' }
       ]"
     >
       <el-input
